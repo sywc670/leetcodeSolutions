a04ead9cecacddfd6a3c414cd38dae1d887c37d5
sywc670 <670886335@qq.com>
2024-05-18 14:18:13 +0800

    feat: update

--- go/code/temp.go
+++ go/code/temp.go
@@ -1,17 +1,1 @@
 package code
-
-func isSubsequence(s string, t string) bool {
-	sp, tp := 0, 0
-	for sp < len(s) && tp < len(t) {
-		if s[sp] == t[sp] {
-			sp++
-			tp++
-			continue
-		}
-		tp++
-	}
-	if sp == len(s) && tp <= len(t) {
-		return true
-	}
-	return false
-}
--- go/code/双指针.go
+++ go/code/双指针.go
@@ -39,3 +39,19 @@ func compress(chars []byte) (ans int) {
 	summary()
 	return
 }
+
+func isSubsequence(s string, t string) bool {
+	sp, tp := 0, 0
+	for sp < len(s) && tp < len(t) {
+		if s[sp] == t[tp] {
+			sp++
+			tp++
+			continue
+		}
+		tp++
+	}
+	if sp == len(s) && tp <= len(t) {
+		return true
+	}
+	return false
+}
